4fdd5c962b677cff183b6af6f22dad54debc9822
antonio <antoniogamiz10@gmail.com>
2018-04-20 12:45:49 +0200

    Aprendiendo sobre los objectos :DDD

---
+++ More things/program.js
@@ -0,0 +1,27 @@
+var empty_object  = {};
+
+var stooge = {
+    "first-name" : "Jerome",
+    "last-name" : "Howard"
+};
+
+document.writeln(stooge);
+document.writeln(stooge["first-name"]);
+
+
+var flight = {
+    airline: "Oceanic",
+    number: 815,
+    departure: {
+    IATA: "SYD",
+    time: "2004-09-22 14:55",
+    city: "Sydney"
+    },
+    arrival: {
+    IATA: "LAX",
+    time: "2004-09-23 10:42",
+    city: "Los Angeles"
+    }
+};
+
+document.writeln(flight.departure.city);
